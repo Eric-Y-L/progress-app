526998d6a1ca6d68c63722c1036843c00700e251
Eric-Y-L <122017878+Eric-Y-L@users.noreply.github.com>
2023-07-26 15:26:56 +0800

    ID

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  client_id: "<YOUR_CLIENT_ID>",
+  client_id: "4c3e4ada1543eabb5981aa6f034ddb76",
   server_url:
     "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
   account_url: "https://accounts.livechatinc.com/"
